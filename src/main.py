99215e72b91cf9432f989e5495d063b6cf1e187f
samdbeckham <sam@beckham.io>
2025-05-10 20:54:05 +0100

    Adds a textnode class

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    textNode = TextNode("test text", "BOLD", "https://google.com")
+    print(textNode)
+
+main()
+
---
+++ src/textnode.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "NORMAL"
+    BOLD = "BOLD"
+    ITALIC = "ITALIC"
+    CODE = "CODE"
+    LINK = "LINK"
+    IMAGE = "IMAGE"
+
+class TextNode():
+    def __init__(self, text, text_type, url = None):
+        self.text = text
+        self.text_type = TextType(text_type)
+        self.url = url
+
+    def __eq__(self, other):
+        return (self.text == other.text and self.text_type.value == other.text_type.value and self.url == other.url)
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
