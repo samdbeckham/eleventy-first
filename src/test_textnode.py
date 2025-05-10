e4304d975545efab86042254d139ed63d4b20c91
samdbeckham <sam@beckham.io>
2025-05-10 21:04:38 +0100

    Adds basic tests

---
+++ src/test_textnode.py
@@ -0,0 +1,32 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_no_url(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://example.com")
+        self.assertNotEqual(node, node2)
+
+    def test_non_matching_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, TEXT, https://www.boot.dev)", repr(node)
+        )
+
+        
+
+
+if __name__ == "__main__":
+    unitest.main()
+
