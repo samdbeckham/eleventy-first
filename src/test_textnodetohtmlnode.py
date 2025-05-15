471159546adae5b05561350d574126aa84e9611e
samdbeckham <sam@beckham.io>
2025-05-15 10:26:05 +0100

    Adds a textnode to htmlnode converter

---
+++ src/test_textnodetohtmlnode.py
@@ -0,0 +1,47 @@
+import unittest
+
+from textnode import TextNode, TextType
+from textnodetohtmlnode import text_node_to_html_node
+
+class TestTextNodeToHtmlNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'b')
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_italic(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'i')
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_code(self):
+        node = TextNode("This is a text node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'code')
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_link(self):
+        node = TextNode("This is a text node", TextType.LINK, "http://example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'a')
+        self.assertEqual(html_node.value, "This is a text node")
+        self.assertEqual(html_node.props, {"href": "http://example.com"})
+
+    def test_image(self):
+        node = TextNode("This is a text node", TextType.IMAGE, "http://example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'img')
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "http://example.com", "alt": "This is a text node"})
+
+if __name__ == "__main__":
+    unitest.main()
+
