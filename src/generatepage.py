db2ddfe70b516afdc84cd2c9b0de8b07c02e7011
samdbeckham <sam@beckham.io>
2025-05-18 08:46:53 +0100

    Adds a generator and some content

---
+++ src/generatepage.py
@@ -0,0 +1,23 @@
+import os
+from markdowntohtmlnode import markdown_to_html_node
+from extracttitle import extract_title
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    markdown_file = open(from_path)
+    template_file = open(template_path)
+    markdown = markdown_file.read()
+    template = template_file.read()
+
+    title = extract_title(markdown)
+    content = markdown_to_html_node(markdown).to_html()
+    html = template.replace("{{ Title }}", title).replace("{{ Content }}", content)
+
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    new_file = open(dest_path, "x")
+    new_file.write(html)
+
+    markdown_file.close()
+    template_file.close()
+    new_file.close()
+
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from textnode import TextNode
 from copy_files import copy_files
+from generatepage import generate_page
 
 def main():
     copy_files("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html") 
 
 main()
 
