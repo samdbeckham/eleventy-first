d00ad6fdc374561f5a46c46e7a9049b44cf1cc80
samdbeckham <sam@beckham.io>
2025-05-19 08:09:46 +0100

    Moved build to docs for gh pages

--- src/main.py
+++ src/main.py
@@ -6,8 +6,8 @@ from generatepagesrecursive import generate_pages_recursive
 def main():
     basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
     print(f"serving files from {basepath}")
-    copy_files("static", "public")
-    generate_pages_recursive("content", "template.html", "public", basepath) 
+    copy_files("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath) 
 
 main()
 
